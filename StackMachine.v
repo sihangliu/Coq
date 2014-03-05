8f5dee256efa1d8b28e751ba37d242dcfccd561f
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2014-03-05 14:01:07 +0530

    Added more defintions

--- StackMachine.v
+++ StackMachine.v
@@ -7,34 +7,36 @@ Inductive exp : Set :=
   | Const : nat -> exp
   | Binop : binop -> exp -> exp -> exp.
 
-Definition binopDenote ( b : binop ) : nat -> nat -> nat := 
-  match b with
+ 
+Definition binopDenot ( b : binop ) : nat -> nat -> nat := 
+  match b with 
       | Plus => plus
       | Times => mult
   end.
 
-Fixpoint expDenote ( e : exp ) : nat := 
-  match e with 
-      | Const n => n
-      | Binop b e1 e2 => ( binopDenote b ) ( expDenote e1 ) ( expDenote e2 )
-  end.
 
-Eval simpl in expDenote ( Binop Times ( Const 2 ) ( Const 4 ) ).
+Fixpoint expDenote ( e : exp ) : nat := 
+   match e with
+    | Const n => n
+    | Binop b e1 e2 => ( binopDenot b ) ( expDenote e1 ) ( expDenote e2 )
+   end.
+
+Eval simpl in expDenote ( Const 42 ).
+Eval simpl in expDenote ( Binop Plus ( Const 2 ) ( Const 2 )). 
+Eval simpl in expDenote ( Binop Times ( Binop Plus ( Const 2 ) ( Const 2 )) ( Const 7)).
 
 Inductive instr : Set := 
-  | iConst : nat -> instr 
+  | iConst : nat -> instr
   | iBinop : binop -> instr.
 
 Definition prog := list instr.
 Definition stack := list nat.
 
-
-Definition instrDenot ( i : instr ) ( s : stack ) : option stack :=
+Definition ( i : instr ) ( s : stack ) : option stack := 
   match i with 
-      | iConst n =>  Some ( n :: s )
-      | iBinop b => 
-        match s with 
-            | t1 :: t2 :: remain => Some ( ( binopDenote b ) t1 t2 :: remain )
-            | _ => None
-        end
+      | iConst n -> Some ( n :: s )
+      | iBinop b => match b with 
+                        | arg1 :: arg2 :: s' => Some ( ( binopDenote b ) arg1 arg2 :: s' )
+                        | _ => None
+                    end
   end.
