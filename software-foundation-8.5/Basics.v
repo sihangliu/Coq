227d2641183becc6386c584093ae004002b9f90d
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-06-30 12:30:22 +1000

    Finished Basic.v

--- software-foundation-8.5/Basics.v
+++ software-foundation-8.5/Basics.v
@@ -280,3 +280,252 @@ Theorem plus_O_n : forall (n : nat), O + n = n.
 Proof.
   intros n. simpl. reflexivity.
 Qed.
+
+(* These are all things that can be applied to a number to yield a number. However, 
+there is a fundamental difference between the first one and the other two: functions 
+like pred and minustwo come with computation rules — e.g., the definition of pred 
+says that pred 2 can be simplified to 1 — while the definition of S has no such behavior 
+attached. Although it is like a function in the sense that it can be applied to an 
+argument, it does not do anything at all! *)
+
+Theorem plus_1_l : forall (n : nat), 1 + n = S n.
+Proof.
+  intros n. simpl. reflexivity.
+Qed.
+
+Theorem mult_0_n : forall (n : nat), O * n = O.
+Proof.
+  intros n. simpl. reflexivity.
+Qed.
+
+Theorem plus_n_O : forall (n : nat), n + O = n.
+Proof.
+  intros n. simpl.
+Abort.
+
+Theorem plus_id_example : forall (n m : nat),
+    n = m -> n + n = m + m.
+Proof.
+  intros n m H. rewrite -> H. reflexivity.
+Qed.
+
+Theorem plus_id_exercise : forall (n m o : nat),
+    n = m -> m = o -> n + m = m + o.
+Proof.
+  intros n m o H1 H2.
+  rewrite -> H1. rewrite <- H2.
+  reflexivity.
+Qed.
+
+Theorem mult_0_plus : forall (n m : nat),
+    (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  rewrite plus_O_n. reflexivity.
+Qed.
+
+Theorem mult_S_1 : forall (n m : nat),
+    m = S n -> m * (1 + n) = m * m.
+Proof.
+  intros n m H.
+  rewrite plus_1_l. rewrite <- H.
+  reflexivity.
+Qed.
+
+Theorem plus_1_neq_0_firsttry : forall (n : nat),
+    beq_nat (n + 1) 0 = false.
+Proof.
+  intros n. simpl.
+Abort.
+
+Theorem plus_1_neq_0 : forall (n : nat),
+    beq_nat (n + 1) 0 = false.
+Proof.
+  intros n. destruct n as [ | n'].
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Theorem negb_involutive : forall (b : bool),
+    negb (negb b) = b.
+Proof.
+  intros b. destruct b.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Theorem andb_commutative : forall (b c : bool),
+    andb b c = andb c b.
+Proof.
+  intros b. destruct b.
+  - intros c. destruct c.
+    + reflexivity.
+    + reflexivity.
+  - intros c. destruct c.
+    + reflexivity.
+    + reflexivity.
+Qed.
+
+Theorem andb_commutative' : forall (b c : bool),
+    andb b c = andb c b.
+Proof.
+  intros b. destruct b.
+  {
+    intros c. destruct c.
+    { reflexivity. }
+    { reflexivity. }
+  }
+  {
+    intros c. destruct c.
+    { reflexivity. }
+    { reflexivity. }
+  }
+Qed.
+
+Theorem andb3_exchange : forall (b c d : bool),
+    andb (andb b c) d = andb (andb b d) c.
+Proof.
+  intros b. destruct b.
+  {
+    intros c. destruct c.
+    {
+      intros d. destruct d.
+      + reflexivity.
+      + reflexivity.
+    }
+    {
+      intros d. destruct d.
+      + reflexivity.
+      + reflexivity.
+  }}
+  {
+    intros c. destruct c.
+    {
+      intros d. destruct d.
+      + reflexivity.
+      + reflexivity.
+    }
+    {
+      intros d. destruct d.
+      + reflexivity.
+      + reflexivity.
+    }}
+Qed.
+
+Theorem plus_1_neg_0' : forall (n : nat),
+    beq_nat (n + 1) 0 = false.
+Proof.
+  intros [|n].
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
+
+Theorem andb_commutative'' : forall (b c : bool),
+    andb b c = andb c b.
+Proof.
+  intros [] [].
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+  + reflexivity.
+Qed.
+
+Theorem andb_true_elim2 : forall (b c : bool),
+    andb b c = true -> c = true.
+Proof.
+  intros [] [].
+  + auto.
+  + auto.
+  + auto.
+  + auto.
+Qed.
+
+Theorem andb_true_elim3 : forall (b c : bool),
+    andb b c = true -> b = true /\ c = true.
+Proof.
+  intros [] [].
+  + auto.
+  + auto.
+  + auto.
+  + auto.
+Qed.
+
+Notation "x + y" := (plus x y)
+                       (at level 50, left associativity)
+                       : nat_scope.
+Notation "x * y" := (mult x y)
+                       (at level 40, left associativity)
+                    : nat_scope.
+
+Theorem identity_fn_applied_twice :
+  forall (f : bool -> bool), (forall (x : bool), f x = x) ->
+                 forall (b : bool), f (f b) = b.
+Proof.
+  intros f H b. rewrite H. apply H with ( x := b).
+Qed.
+
+Theorem negation_fn_applied_twice :
+  forall (f : bool -> bool), (forall (x : bool), f x = negb x) ->
+                 forall (b : bool), f (f b) = b.
+Proof.
+  intros f H b. rewrite H. rewrite H.
+  rewrite negb_involutive. reflexivity.
+Qed.
+
+Theorem andb_eq_orb : forall (b c : bool), (andb b c = orb b c) -> b = c.
+Proof.
+  intros [] [].
+  + auto.
+  + auto.
+  + auto.
+  + auto.
+Qed.
+
+Inductive bin :=
+| Zero : bin
+| D : bin -> bin (* 2 * n *)
+| T : bin -> bin. (* 2 * n + 1 *) 
+
+Check Zero. (* 0 *)
+Check T Zero. (* 1 *)
+Check D (T Zero). (* 2 *)
+Check T (T Zero). (* 3 *)
+Check D (D (T Zero)). (* 4 *)
+Check T (D (T Zero)). (* 5 *)
+Check D (T (T Zero)). (* 6 *)
+
+Fixpoint incr (b : bin) : bin :=
+  match b with
+  | Zero => T Zero
+  | D n => T n
+  | T n => D (incr n)
+  end.
+
+Compute (incr Zero).
+Compute (incr (incr Zero)).
+Compute (incr (incr (incr Zero))).
+Compute (incr (incr (incr (incr Zero)))).
+Compute (incr (incr (incr (incr (incr Zero))))).
+Compute (incr (incr (incr (incr (incr (incr Zero)))))).
+Compute (incr (incr (incr (incr (incr (incr (incr Zero))))))).
+
+Fixpoint bin_to_nat (b : bin) : nat :=
+  match b with
+  | Zero => O
+  | D n => let v := bin_to_nat n in plus v v
+  | T n => let v := bin_to_nat n in plus 1 (plus v v)
+  end.                     
+
+Compute (bin_to_nat (D (T (T Zero)))).
+Compute (bin_to_nat (D (T Zero))).  
+
+Theorem bin_correctness :
+  forall (b : bin), plus 1 (bin_to_nat b) = bin_to_nat (incr b).  
+Proof.
+  intros b. induction b.
+  + auto.
+  + auto.
+  + simpl. rewrite <- IHb. Require Import Omega.
+    omega.
+Qed.
+
