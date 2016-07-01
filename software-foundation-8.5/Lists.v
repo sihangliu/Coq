e52f7b9cefbf07e0b24e8cdf8ccf6f6b25b18fa9
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-07-01 15:45:55 +1000

    finished oddmembers

--- software-foundation-8.5/Lists.v
+++ software-foundation-8.5/Lists.v
@@ -86,4 +86,33 @@ Module Natlist.
     nonzeros [0;1;0;2;3;0;0] = [1;2;3].
   Proof. auto. Qed.
 
+  Fixpoint oddmembers (l : natlist) : natlist :=
+    match l with
+    | nil => nil
+    | h :: t =>
+      if evenb h then oddmembers t else h :: oddmembers t
+    end.
+
+  Example test_oddmembers: oddmembers [0;1;0;2;3;0;0] = [1;3].
+  Proof. simpl. reflexivity. Qed.
+
+  Fixpoint countoddmembers (l : natlist) : nat :=
+    match l with
+    | nil => O
+    | h :: t => if evenb h then countoddmembers t else S (countoddmembers t)
+    end.
+
+  
+  Example test_countoddmembers1:
+    countoddmembers [1;0;3;1;4;5] = 4.
+  Proof. auto. Qed.
+
+  Example test_countoddmembers2:
+    countoddmembers [0;2;4] = 0.
+  Proof. auto. Qed.
+
+  Example test_countoddmembers3:
+    countoddmembers nil = 0.
+  Proof. auto. Qed.
+
   
