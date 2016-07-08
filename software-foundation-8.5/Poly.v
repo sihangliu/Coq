122274dd512e011b45e8828886315d29dcb35346
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-07-08 14:03:57 +1000

    finished curry_uncurry

--- software-foundation-8.5/Poly.v
+++ software-foundation-8.5/Poly.v
@@ -338,3 +338,188 @@ Proof.
     rewrite H. rewrite IHl. reflexivity.
 Qed.
 
+Fixpoint flat_map {X Y : Type} (f : X -> list Y) (l : list X) : list Y :=
+  match l with
+  | nil => nil
+  | h :: t => f h ++ flat_map f t
+  end.
+
+Example test_flat_map1:
+  flat_map (fun n => [n;n;n]) [1;5;4] = [1; 1; 1; 5; 5; 5; 4; 4; 4].
+Proof. auto. Qed.
+
+Definition option_map {X Y : Type} (f : X -> Y) (x : option X) : option Y :=
+  match x with
+  | None => None
+  | Some a => Some (f a)
+  end.
+
+
+
+Module NoImplicitTypes.
+  Fixpoint filter (X : Type) (f : X -> bool) (l : list X) : list X :=
+    match l with
+    | nil => nil
+    | h :: t => if f h then h :: filter X f t else filter X f t
+    end.
+
+  Fixpoint map (X Y : Type) (f : X -> Y) (l : list X) : list Y :=
+    match l with
+    | nil => nil
+    | h :: t =>
+      f h :: map X Y f t
+    end.
+
+  Example test_filter1: filter nat evenb [1;2;3;4] = [2;4].
+  Proof. auto. Qed.
+
+  Example test_filter2':
+    filter (list _) (fun l => Basics.beq_nat (length l) 1)
+           [ [1; 2]; [3]; [4]; [5;6;7]; []; [8] ] = [ [3]; [4]; [8] ].
+  Proof. compute. reflexivity. Qed.
+  Check @test_filter2'.
+
+  Example test_map1: map nat nat (fun x => plus 3 x) [2;0;2] = [5;3;5].
+  Proof. auto. Qed.
+
+  Example test_map2: map nat bool oddb [2;1;2;5] = [false;true;false;true].
+  Proof. auto. Qed.
+
+  Example test_map3:
+    map nat (list bool) (fun n => [evenb n;oddb n]) [2;1;2;5]
+    = [[true;false];[false;true];[true;false];[false;true]].
+  Proof. auto. Qed.
+
+End NoImplicitTypes.
+
+Fixpoint fold {X Y : Type} (f : X -> Y -> Y) (l : list X) (b : Y) : Y :=
+  match l with
+  | nil => b
+  | h :: t => f h (fold f t b) (* fold f t (f h b) *)
+  end.
+
+Check (fold andb).
+
+Example fold_example1 :
+  fold mult [1;2;3;4] 1 = 24.
+Proof. compute. auto. Qed.
+
+Example fold_example2 :
+  fold andb [true;true;false;true] true = false.
+Proof. auto. Qed.
+
+
+Example fold_example3 :
+  fold app [[1];[];[2;3];[4]] [] = [1;2;3;4].
+Proof. auto. Qed.
+
+Definition constfun {X : Type} (x : X) : nat -> X :=
+  fun (k : nat) => x.
+
+Definition ftrue := constfun true.
+
+Example constfun_example1 : ftrue 0 = true.
+Proof. auto. Qed.
+
+Example constfun_example2 : (constfun 5) 99 = 5.
+Proof. auto. Qed.
+
+Check plus.
+
+Module Vector.
+  Inductive Vect (X : Type) : nat -> Type :=
+  | nil : Vect X O
+  | cons : forall (n : nat),  X ->  Vect X n -> Vect X (S n).
+
+  Check @nil.
+  Check @cons.
+  Arguments nil {X}.
+  Arguments cons {X} {n} _ _.
+  Check cons 1 (cons 2 nil).
+
+  Fixpoint app {X : Type} {n m : nat} (lx : Vect X n) (ly : Vect X m) : Vect X (n + m) :=
+    match lx with
+    | nil => ly
+    | cons h t => cons h (app t ly)
+    end. 
+
+  Fixpoint map {X Y : Type} {n : nat} (f : X -> Y) (lx : Vect X n) : Vect Y n :=
+    match lx with
+    | nil => nil
+    | cons h t => cons (f h) (map f t)
+    end.                   
+    
+    
+  Definition zip {X Y : Type} {n : nat} (lx : Vect X n) (ly : Vect Y n) : Vect (X*Y) n.
+  Proof.
+    destruct lx.
+    constructor.
+    induction ly.
+    constructor.
+    apply cons.
+    exact (x, x0).
+    exact IHly.
+  Defined.
+
+  Definition filter {X : Type} {n : nat} (l : Vect X n) : {p : {m : nat & Vect X m} | projT1 p <= n}.
+  Proof. induction n.
+  Abort.
+  
+End Vector.
+
+
+Definition fold_length {X : Type} (l : list X) : nat :=
+  fold (fun _ n => S n) l 0.
+
+Example test_fold_length1 : fold_length [4;7;0] = 3.
+Proof. auto. Qed.
+
+Theorem fold_length_correct : forall (X : Type) (l : list X),
+  fold_length l = length l.
+Proof.
+  intros X l. induction l.
+  + auto.
+  + simpl. unfold fold_length. simpl.
+    unfold fold_length in IHl. rewrite IHl. reflexivity.
+Qed.
+
+Definition fold_map {X Y:Type} (f : X -> Y) (l : list X) : list Y :=
+  fold (fun x acc => f x :: acc) l nil.
+
+Theorem map_fold :
+  forall (X Y : Type)(f : X -> Y) (l : list X), map f l = fold_map f l.
+Proof.
+  intros X Y f l. induction l.
+  + auto.
+  + simpl. unfold fold_map.
+    simpl. unfold fold_map in IHl. rewrite <- IHl.
+    reflexivity.
+Qed.
+
+Definition prod_curry {X Y Z : Type}
+           (f : X * Y -> Z) (x : X) (y : Y) : Z := f (x, y).
+
+Definition prod_uncurry {X Y Z : Type}
+           (f : X -> Y -> Z) (p : X * Y) : Z := f (fst p) (snd p).
+
+Example test_map22: map (fun x => plus 3 x) [2;0;2] = [5;3;5].
+Proof. auto. Qed.
+
+Check @prod_curry.
+Check @prod_uncurry.
+
+Theorem uncurry_curry : forall (X Y Z : Type) (f : X -> Y -> Z) x y,
+    prod_curry (prod_uncurry f) x y = f x y.
+Proof.
+  intros X Y Z f x y. unfold prod_uncurry.
+  unfold prod_curry. simpl. reflexivity.
+Qed.
+
+Theorem curry_uncurry : forall (X Y Z : Type) (f : (X * Y) -> Z) (p : X * Y),
+    prod_uncurry (prod_curry f) p = f p.
+Proof.
+  intros X Y Z f p. unfold prod_curry.
+  unfold prod_uncurry. destruct p.
+  simpl. reflexivity.
+Qed.
+
