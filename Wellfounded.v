064a6b677fbfa7e8a7ebb516be9deb51d30c1ee2
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-07-25 17:23:50 +1000

    using program tactic

--- Wellfounded.v
+++ Wellfounded.v
@@ -1,4 +1,5 @@
 Require Import Omega.
+Require Import Coq.Program.Wf.
 
 Definition minus_decrease :
   forall x y : nat, Acc lt x -> x <> 0 -> y <> 0 -> Acc lt (x - y).
@@ -18,4 +19,20 @@ Proof.
   apply (minus_decrease x y H n n0).
 Qed.
 
+Definition div x y := div_aux x y (lt_wf x).
+Print lt_wf.
 
+Program Fixpoint div_aux' (x y : nat) (H : y <> 0) {measure  x} : nat :=
+  if eq_nat_dec x 0 then 0
+  else S (div_aux' (x - y) y _).
+Next Obligation.
+  omega.
+Qed.
+
+Lemma not30 : 3 <> 0.
+Proof.
+  omega.
+Qed.
+
+Compute (div_aux' 6 3 not30).
+Print div_aux'.
