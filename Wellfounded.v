648e7623d7a85a879de714b7ca7de59769780e62
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-07-27 14:24:07 +1000

    added customised induction principal for nat

--- Wellfounded.v
+++ Wellfounded.v
@@ -62,7 +62,7 @@ Compute  (remainder 10 3 not30).
 
 Inductive rem : nat -> nat -> nat -> Prop :=
 | cons_0 n1 n2 : n1 < n2 -> rem n1 n2 n1
-| cons_1 n1 n2 n3 : n1 >= n2 -> rem (n1 - n2) n2 n3 -> rem n1 n2 n3.  
+| cons_1 n1 n2 n3 : rem n1 n2 n3 -> rem (n1 + n2) n2 n3.  
 
 Lemma rem_1 : rem 2 4 2. 
 Proof.
@@ -71,36 +71,35 @@ Qed.
 
 Lemma rem_2 : rem 10 4 2.
 Proof.  
-  constructor 2. omega.
-  simpl.  constructor 2. omega.
-  simpl. constructor 1. omega.
+  constructor 2 with (n1 := 6).
+  simpl.  constructor 2 with (n1 := 2).
+  constructor 1. omega.
 Qed.
 
 Lemma rem_3 : forall n : nat, n <> 0 -> rem n n 0.
 Proof.
-  intros. constructor 2. omega.
-  assert (H1 : n - n = 0) by omega.
-  rewrite H1. constructor 1.  omega.
+  intros. constructor 2 with (n1 := 0).
+  constructor 1.  omega.
 Qed.
 
 Lemma rem_4 : forall n m p : nat, p <> 0 -> rem n m p -> rem (n + m) m p.
 Proof.
-  constructor 2. omega.
-  assert (H1 : n + m - m = n) by omega.
-  rewrite H1. trivial.
+  intros. apply (cons_1 n m p H0).
 Qed.
 
 Inductive gcd : nat -> nat -> nat -> Prop :=
 |cons_a a b : a = b -> gcd a b b
-|cons_b a b c : b > a -> gcd a (b - a) c -> gcd a b c
-|cons_c a b c : a > b -> gcd (a - b) b c -> gcd a b c. 
+|cons_b a b c : b > a -> gcd a (b - a) c -> gcd a b c  (* b > a *)
+|cons_c a b c : a > b -> gcd (a - b) b c -> gcd a b c. (* a > b *)
 
 Lemma gcd_0 : gcd 10 3 1.
 Proof.
-  constructor 3. omega. constructor 3. omega. simpl.
-  constructor 3. omega. simpl. constructor 2. omega.
-  simpl. constructor 2. omega. simpl. constructor 1.
-  auto.
+  constructor 3. omega. simpl.
+  constructor 3. omega. simpl.
+  constructor 3. omega. simpl.
+  constructor 2. omega. simpl.
+  constructor 2. omega. simpl.
+  constructor 1. auto.
 Qed.
 
 Print gcd_0.
@@ -110,4 +109,62 @@ Proof.
   intros. constructor 1. auto.
 Qed.
 
-  
+
+
+Inductive pow2n : nat -> nat -> Prop :=
+| val_0 : pow2n 0 1
+| val_n n m : pow2n n m -> pow2n (S n) (2 * m).
+
+
+Lemma pow_0 : pow2n 3 8.
+Proof.
+  constructor 2 with (n := 2) (m := 4).
+  constructor 2 with (n := 1) (m := 2).
+  constructor 2 with (n := 0) (m := 1).
+  constructor 1.
+Qed.
+
+SearchAbout (_ <=? _).
+Print pow_0.
+
+Inductive expo : nat -> nat -> nat -> Prop :=
+| pval_0 n : expo n 0 1
+| pval_1 n m p : expo n m p -> expo n (S m) (n * p).
+
+Lemma expo_1 : expo 2 4 16.
+Proof.
+  constructor 2 with (n := 2) (m := 3) (p := 8).
+  constructor 2 with (n := 2) (m := 2) (p := 4).
+  constructor 2 with (n := 2) (m := 1) (p := 2).
+  constructor 2 with (n := 2) (m := 0) (p := 1).
+  constructor 1 with (n := 2).
+Qed.
+
+Print expo_1.
+
+Lemma mod_add_back: forall n m : nat, m <> 0 -> ((n + m) mod m) = (n mod m).
+Proof.
+  intros. SearchAbout ((_ + _) mod _ = _ mod _).
+  rewrite Nat.add_mod. SearchAbout (_ mod _ = 0).
+  rewrite Nat.mod_same. rewrite plus_0_r.
+  SearchAbout ((_ mod _) mod _). rewrite Nat.mod_mod.
+  auto. auto. auto. auto.
+Qed.
+
+Print mod_add_back.
+
+Fixpoint div2 (n : nat) : nat :=
+  match n with
+  | 0 => 0
+  | 1 => 0
+  | S (S n') => S (div2 n')
+  end.
+
+Lemma nat_ind_alt : forall P : nat -> Prop,
+  P 0 -> P 1 -> (forall n, P n -> P (S (S n))) -> forall n, P n.
+Proof.
+  intros P H1 H2 H3. Check lt_wf_ind.
+  induction n as  [[| [| n]] H4] using lt_wf_ind.
+  auto. auto. auto.
+Qed.
+
