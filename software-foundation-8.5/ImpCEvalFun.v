640196f44f1787bb4e350073dee93a9e5b31c9d3
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2017-01-06 13:01:46 +1100

    finishd ceval_step3 function

--- software-foundation-8.5/ImpCEvalFun.v
+++ software-foundation-8.5/ImpCEvalFun.v
@@ -18,5 +18,56 @@ Fixpoint ceval_step1 (st : state) (c : com) : state :=
   end.
 
 
+Fixpoint ceval_step2 (st : state) (c : com) (i : nat) : state :=
+  match i with
+  | 0 => empty_state
+  | S i' =>
+       match c with
+       | SKIP => st
+       | l ::= a1 =>
+         t_update st l (aeval st a1)
+       | c1 ;; c2 =>
+         let st' := ceval_step2 st c1 i' in
+         ceval_step2 st' c2 i' 
+       | IFB b THEN c1 ELSE c2 FI =>
+         if (beval st b) then ceval_step2 st c1 i'
+         else ceval_step2 st c2 i'
+       | WHILE b1 DO c1 END =>
+         if (beval st b1) then
+           let st' := ceval_step2 st c1 i' in
+           ceval_step2 st' c i'
+         else st
+       end 
+  end.
+
+
+Fixpoint ceval_step3 (st : state) (c : com) (i : nat) : option state :=
+  match i with
+  | 0 => None
+  | S i' =>
+    match c with
+    | SKIP => Some st
+    | l ::= a1 =>
+      Some (t_update st l (aeval st a1))
+    | c1;; c2 =>
+      match ceval_step3 st c1 i' with
+      | None => None
+      | Some st' => ceval_step3 st' c2 i'
+      end
+    | IFB b THEN c1 ELSE c2 FI =>
+      if (beval st b) then ceval_step3 st c1 i'
+      else ceval_step3 st c2 i'
+    | WHILE b1 DO c1 END =>
+      if (beval st b1) then
+        match ceval_step3 st c1 i' with
+        | None => None
+        | Some st' => ceval_step3 st' c i'
+        end
+      else Some st
+    end
+  end.
+
+
+        
       
 
