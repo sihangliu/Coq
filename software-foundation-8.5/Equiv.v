e82274ebccd6a260a15e516e761b0264bfbbf40c
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2017-01-09 13:56:15 +1100

    Added definitions

--- software-foundation-8.5/Equiv.v
+++ software-foundation-8.5/Equiv.v
@@ -9,3 +9,12 @@ Require Import Maps.
 Require Import Imp.
 
 
+Definition aequiv (a1 a2 : aexp) : Prop :=
+  forall (st : state), aeval st a1 = aeval st a2.
+
+Definition bequiv (b1 b2 : bexp) : Prop :=
+  forall (st : state), beval st b1 = beval st b2.
+
+Definition cequiv (c1 c2 : com) : Prop :=
+  forall (st st' : state), (c1 / st \\ st') <-> (c2 / st \\ st'). 
+
