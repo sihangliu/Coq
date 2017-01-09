6e4fb55adf9f2341fc6a25f0a802846984caef68
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2017-01-09 13:21:53 +1100

    started Equivalent chapter

---
+++ software-foundation-8.5/Equiv.v
@@ -0,0 +1,11 @@
+Require Import Coq.Bool.Bool.
+Require Import Coq.Arith.Arith.
+Require Import Coq.Arith.EqNat.
+Require Import Coq.omega.Omega.
+Require Import Coq.Lists.List.
+Require Import Coq.Logic.FunctionalExtensionality.
+Import ListNotations.
+Require Import Maps.
+Require Import Imp.
+
+
