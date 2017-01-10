6c908a4c3124f75395097222acae8ff0ee368aab
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2017-01-10 13:52:32 +1100

    finished swap_if_branches

--- software-foundation-8.5/Equiv.v
+++ software-foundation-8.5/Equiv.v
@@ -82,3 +82,75 @@ Definition prog_i : com :=
 Definition equiv_classes : list (list com) :=
   [[prog_a; prog_f; prog_g]; [prog_b; prog_e]; [prog_c; prog_d; prog_h]; [prog_i]].
 
+Theorem aequiv_example:
+  aequiv (AMinus (AId X) (AId X)) (ANum 0).
+Proof.
+  intros st. simpl. omega.
+Qed.
+
+Theorem bequiv_example:
+  bequiv (BEq (AMinus (AId X) (AId X)) (ANum 0)) BTrue.
+Proof.
+  intros st. unfold beval.
+  rewrite aequiv_example. reflexivity.
+Qed.
+
+Theorem skip_left: forall c, cequiv (SKIP;; c) c.
+Proof.
+  unfold cequiv. intros c st st'. split.
+  intros H. inversion H. subst.
+  inversion H2.  subst. assumption.
+  intros H. apply E_Seq with st. apply E_Skip.
+  assumption.
+Qed.
+
+Theorem skip_right: forall c, cequiv (c ;; SKIP) c.
+Proof.
+  unfold cequiv. intros c st st'. split.
+  intros H. inversion H. subst. inversion H5. subst.
+  assumption.
+  intros H. apply E_Seq with st'. assumption.
+  apply E_Skip.
+Qed.
+
+Theorem IFB_true_simple: forall c1 c2,
+    cequiv (IFB BTrue THEN c1 ELSE c2 FI)
+           c1.
+Proof.
+  unfold cequiv. intros c1 c2 st st'.
+  split. intros H. inversion H. subst.
+  assumption. subst. inversion H5.
+  intros H. apply E_IfTrue. auto.
+  assumption.
+Qed.
+
+Theorem IFB_false: forall b c1 c2,
+    bequiv b BFalse ->
+    cequiv
+      (IFB b THEN c1 ELSE c2 FI)
+      c2.
+Proof.
+  unfold bequiv, cequiv. intros. split; intros.
+  inversion H0; subst. simpl in H.
+  specialize (H st). rewrite H6 in H. inversion H.
+  assumption.
+  simpl in H. apply E_IfFalse. specialize (H st).
+  assumption. assumption.
+Qed.
+
+Theorem swap_if_branches: forall b e1 e2,
+    cequiv
+      (IFB b THEN e1 ELSE e2 FI)
+      (IFB BNot b THEN e2 ELSE e1 FI).
+Proof.
+  unfold cequiv. intros. split; intros.
+  inversion H; subst. apply E_IfFalse. simpl.
+  apply  negb_false_iff. auto. auto.
+  apply E_IfTrue. simpl. apply negb_true_iff. auto. auto.
+
+  inversion H; subst. simpl in H5. apply negb_true_iff in H5.
+  apply E_IfFalse. auto. auto.
+  simpl in H5. apply negb_false_iff in H5. apply E_IfTrue. auto.
+  auto.
+Qed.
+
