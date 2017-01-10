e66ea182ee90b09c95769431d64ead9370a3e640
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2017-01-10 12:20:09 +1100

    finished equiv_classes

--- software-foundation-8.5/Equiv.v
+++ software-foundation-8.5/Equiv.v
@@ -18,3 +18,67 @@ Definition bequiv (b1 b2 : bexp) : Prop :=
 Definition cequiv (c1 c2 : com) : Prop :=
   forall (st st' : state), (c1 / st \\ st') <-> (c2 / st \\ st'). 
 
+(* X := Id 0, Y := Id 1, Z := Id 2 *)
+
+Definition prog_a : com :=
+  WHILE BNot (BLe (AId X) (ANum 0)) DO
+        X ::= APlus (AId X) (ANum 1)
+        END.
+(* while !(x < 0) do x ::= x + 1 
+   Infinite loop *)
+
+Definition prog_b : com :=
+  IFB BEq (AId X) (ANum 0) THEN
+      X ::= APlus (AId X) (ANum 1);;
+      Y ::= ANum 1
+      ELSE
+      Y ::= ANum 0
+      FI;;
+      X ::= AMinus (AId X) (AId Y);;
+      Y ::= ANum 0.
+(* condition true x := 1, y := 1 => x := 0 y := 0 *)  
+
+Definition prog_c : com :=
+  SKIP.
+(* x := 0 y := 1 *)
+
+Definition prog_d : com :=
+  WHILE BNot (BEq (AId X) (ANum 0)) DO
+        X ::= APlus (AMult (AId X) (AId Y)) (ANum 1)
+        END.
+(* condition false because x := 0 so final output is x := 0 y := 1 *)
+
+Definition prog_e : com :=
+  Y ::= ANum 0.
+(* x := 0 y := 0 *)
+
+Definition prog_f : com :=
+  Y ::= APlus (AId X) (ANum 1);;
+    WHILE BNot (BEq (AId X) (AId Y)) DO
+    Y ::= APlus (AId X) (ANum 1)
+    END.
+(* y := 1 => condition true because x := 0 and y := 1 
+   and loop continue because no change in x and y always remains 1 
+   infinite loop *)
+
+Definition prog_g : com :=
+  WHILE BTrue DO
+        SKIP
+        END.
+(* infinite loop *)
+
+Definition prog_h : com :=
+  WHILE BNot (BEq (AId X) (AId X)) DO
+        X ::= APlus (AId X) (ANum 1)
+        END.
+(* condition false and no loop execution. x := 0 and y := 1 *)
+
+Definition prog_i : com :=
+  WHILE BNot (BEq (AId X) (AId Y)) DO
+        X ::= APlus (AId Y) (ANum 1)
+        END.
+(* condition true x := 1 and now condition false x := 1 y := 1 *)
+
+Definition equiv_classes : list (list com) :=
+  [[prog_a; prog_f; prog_g]; [prog_b; prog_e]; [prog_c; prog_d; prog_h]; [prog_i]].
+
